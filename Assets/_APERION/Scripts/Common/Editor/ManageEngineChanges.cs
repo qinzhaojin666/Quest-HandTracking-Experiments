3a2bc82f1f30de41f3f2c14841e985dfefd235f7
tejasVR <tejasshroff24@gmail.com>
2020-02-03 22:45:17 -0500

    adding _APERION repo

---
+++ Assets/_APERION/Scripts/Common/Editor/ManageEngineChanges.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+namespace APERION
+{
+    public class ManageEngineChanges : EditorWindow
+    {
+        [SerializeField] private string xrcorePath = "";
+
+        [MenuItem("APERION/Manage Engine Changes")]
+        private static void ManageChanges()
+        {
+            GetWindow<ManageEngineChanges>();
+        }
+
+        protected void OnEnable()
+        {
+            // Here we retrieve the data if it exists or we save the default field initialisers we set above
+            var data = EditorPrefs.GetString("ManageEngineChanges", JsonUtility.ToJson(this, false));
+            // Then we apply them to this window
+            JsonUtility.FromJsonOverwrite(data, this);
+        }
+
+        protected void OnDisable()
+        {
+            // We get the Json data
+            var data = JsonUtility.ToJson(this, false);
+            // And we save it
+            EditorPrefs.SetString("ManageXRCOREChanges", data);
+        }
+
+        private void OnGUI()
+        {
+            xrcorePath = EditorGUILayout.TextField("XRCORE Path: ", xrcorePath);
+
+            GUILayout.Label("Make sure to link the XRCORE path to the '_APERION' folder within your XRCORE repo");
+
+            if (GUILayout.Button("Browse for Aperion Engine Repo"))
+            {
+                xrcorePath = EditorUtility.OpenFolderPanel("Select Aperion Engine Repo Location", "", "");
+
+                Debug.Log("Hey there, you've set your Aperion Engine repo folder to be: " + xrcorePath);
+            }
+
+            if (GUILayout.Button("Push Engine Changes"))
+            {
+                if (Directory.Exists(xrcorePath))
+                {
+                    FileUtil.ReplaceDirectory("Assets/_APERION", xrcorePath);
+
+                    Debug.Log("Hey there, the files in your _APERION folder have been sent over to your Aperion Engine repo!");
+                }
+            }
+
+            if (GUILayout.Button("Pull Engine Changes"))
+            {
+                if (Directory.Exists(xrcorePath))
+                {
+                    FileUtil.ReplaceDirectory(xrcorePath, "Assets/_APERION");
+
+                    AssetDatabase.Refresh();
+
+                    Debug.Log("Hey there, the files in your Aperion Engine repo have been pulled into your _APERION folder!");
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Assets/_APERION/Scripts/VR/Player/HandModel.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace APERION.VR
+{
+    public class HandModel : MonoBehaviour
+    {
+        public enum HandOrientation
+        {
+            Left,
+            Right
+        }
+
+        [SerializeField] HandOrientation handOrientation;
+        
+        private GameObject handToFollow;
+        private Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void Start()
+        {
+            FindHandObject();
+        }
+
+        private void Update()
+        {
+            MoveRigidbody();
+            // Lerp follow appropriate hand
+            //transform.position = Utility.Vector3Lerp(transform.position, handToFollow.transform.position, 30F);
+        }
+
+        private void FixedUpdate()
+        {
+           
+        }
+
+        private void FindHandObject()
+        {
+            // Get hand via switch statement and enum declaration
+            switch (handOrientation)
+            {
+                case HandOrientation.Left:
+                    handToFollow = PlayerManager.Instance.leftHand;
+                    break;
+
+                case HandOrientation.Right:
+                    handToFollow = PlayerManager.Instance.rightHand;
+                    break;
+            }
+        }
+
+        private void MoveRigidbody()
+        {
+            Vector3 destination = handToFollow.transform.position - transform.position;
+            destination = destination.normalized * Time.deltaTime;
+
+            rb.MovePosition(transform.position + destination);
+        }
+    }
+
+}
+
