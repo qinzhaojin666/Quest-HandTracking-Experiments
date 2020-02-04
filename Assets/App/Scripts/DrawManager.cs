4b7f828f8fcf22bfedcd2222dabdcfa7b9047517
tejasVR <tejasshroff24@gmail.com>
2020-02-03 23:19:24 -0500

    adding line and draw scrips

---
+++ Assets/App/Scripts/DrawManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawManager : MonoBehaviour
+{
+    [SerializeField] GameObject linePrefab;
+
+    private Line activeLine;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject gO = Instantiate(linePrefab);
+            activeLine = gO.GetComponent<Line>();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+
+        if (activeLine != null)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            activeLine.UpdateLine(mousePosition);
+        }       
+    }
+}
---
+++ Assets/App/Scripts/Line.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Line : MonoBehaviour
+{        
+    private LineRenderer lineRenderer;
+    private EdgeCollider2D edgeCollider;
+
+    private List<Vector2> points;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        edgeCollider = GetComponent<EdgeCollider2D>();
+    }
+
+    public void UpdateLine(Vector2 _mousePosition)
+    {
+        if(points == null)
+        {
+            points = new List<Vector2>();
+            SetPoint(_mousePosition);
+            return;
+        }
+
+        if (Vector2.Distance(points.Last(), _mousePosition) > 0.1F)
+        {
+            SetPoint(_mousePosition);
+        }
+    }
+
+    // Check if the mouse has moved enough for us to insert new point
+    // If it has: Insert point at mouse position
+
+    private void SetPoint(Vector2 _point)
+    {
+        points.Add(_point);
+
+        lineRenderer.positionCount = points.Count;        
+
+        lineRenderer.SetPosition(points.Count - 1, _point);
+        if (points.Count > 1)
+        {
+            edgeCollider.points = points.ToArray();
+        }
+    }
+
+          
+}
